36183db7a94ffed1b3e3d8400a34f3acd7b6a8cc
MMirbach <m.mirbach153@gmail.com>
2022-04-24 21:49:41 +0300

    created base class
    created Combatant base class, 2 classes derive from it (Enemy and Playable). Those 2 classes should be the base for all battle participants

---
+++ Assets/Core/CharacterTypes/Combatant.cs
@@ -0,0 +1,65 @@
+using Core.Events;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core
+{
+    public abstract class Combatant : MonoBehaviour, IPointerClickHandler
+    {
+        public int id = 2;
+        public GameEvent endTurnEvent;
+        public GameIntEvent attackEvent;
+        public GameClickEvent clickEvent;
+
+        protected bool MyTurn = false;
+        protected int CurrentDamage;
+        protected Animator Animator;
+        protected static readonly int TriggerAttack = Animator.StringToHash("TriggerAttack");
+        protected static readonly int TriggerAttacked = Animator.StringToHash("TriggerAttacked");
+
+        private void OnEnable()
+        {
+            Animator = GetComponent<Animator>();
+        }
+
+        protected abstract int GetDefense();
+
+        protected abstract int GetDamage();
+
+        // this function should reduce the hp by _currentDamage
+        protected abstract void ReduceHp();
+
+        // this function should trigger when the battle system raises the next turn event
+        public abstract void TurnStarted(int turnId);
+
+        private void EndTurn()
+        {
+            MyTurn = false;
+            endTurnEvent.Raise();
+        }
+
+        private void Attack()
+        {
+            attackEvent.Raise(GetDamage());
+        }
+
+        public void Attacked(int damage)
+        {
+            var relativeDamage = damage - GetDefense();
+            CurrentDamage = relativeDamage > 0 ? relativeDamage : 1;
+            Animator.SetTrigger(TriggerAttacked);
+        }
+
+        private void TakeDamage()
+        {
+            ReduceHp();
+            CurrentDamage = 0;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            clickEvent.Raise(eventData);
+        }
+
+    }
+}
---
+++ Assets/Core/CharacterTypes/Playable.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Core;
+using Core.Stats;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core
+{
+    public class Playable : Combatant
+    {
+        public StatBlock stats;
+
+        protected override int GetDefense()
+        { return stats.defense.value; }
+
+        protected override int GetDamage()
+        { return stats.damage.value; }
+
+        protected override void ReduceHp()
+        { stats.hp.value -= CurrentDamage; }
+
+        public override void TurnStarted(int turnId)
+        {
+            if (id != turnId) return;
+            MyTurn = true;
+        }
+
+        public void HandleClick(PointerEventData eventData)
+        {
+            // detect who was clicked, open menu, menu needs to deal with whatever option is chosen on the clicked target
+            if (!MyTurn) return;
+            if (eventData.pointerPress.TryGetComponent(typeof(EnemyScript), out Component enemyScript))
+            {
+                Animator.SetTrigger(TriggerAttack);
+                //return;
+            }
+        }
+    }
+}
