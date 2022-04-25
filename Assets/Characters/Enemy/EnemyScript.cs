74f553de5b0e25293fa15a7eeed15fcbd8d3e314
MMirbach <70845604+MMirbach@users.noreply.github.com>
2022-04-25 17:47:22 +0300

    Implement combatant interface/base class (#4)
    * created base class
    
    created Combatant base class, 2 classes derive from it (Enemy and Playable). Those 2 classes should be the base for all battle participants

--- Assets/Characters/Enemy/EnemyScript.cs
+++ Assets/Characters/Enemy/EnemyScript.cs
@@ -1,63 +1,9 @@
-using System;
-using Core.Events;
-using UnityEngine;
-using UnityEngine.EventSystems;
+using Core.CharacterTypes;
 
-public class EnemyScript : MonoBehaviour, IPointerClickHandler
+public class EnemyScript : Enemy
 {
-    public int hp;
-    public int energy;
-    
-    public int damage;
-    public int defense;
-    public int speed;
-
-    public GameIntEvent playerAttackedEvent;
-    public GameEvent endTurnEvent;
-    public GameClickEvent clickEvent;
-    
-    private Animator _animator;
-
-    private int _currentDamage;
-    private static readonly int TriggerAttack = Animator.StringToHash("TriggerAttack");
-    private static readonly int TriggerAttacked = Animator.StringToHash("TriggerAttacked");
-
-    private void OnEnable()
+    protected override void PlayTurn()
     {
-        _animator = GetComponent<Animator>();
-    }
-
-    public void PlayTurn(int id)
-    {
-        if (id != 2) return;
-        _animator.SetTrigger(TriggerAttack);
-    }
-
-    private void Attack()
-    {
-        playerAttackedEvent.Raise(damage);
-    }
-
-    private void EndTurn()
-    {
-        endTurnEvent.Raise();
-    }
-    
-    public void Attacked(int damage)
-    {
-        var relativeDamage = damage - defense;
-        _currentDamage = relativeDamage > 0 ? relativeDamage : 1;
-        _animator.SetTrigger(TriggerAttacked);
-    }
-
-    private void TakeDamage()
-    {
-        hp -= _currentDamage;
-        _currentDamage = 0;
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        clickEvent.Raise(eventData);
+        Animator.SetTrigger(TriggerAttack);
     }
 }
---
+++ Assets/Core/CharacterTypes/Enemy.cs
@@ -0,0 +1,37 @@
+
+namespace Core.CharacterTypes
+{
+    public abstract class Enemy : Combatant
+    {
+        public int hp;
+        public int energy;
+
+        public int damage;
+        public int defense;
+        public int speed;
+
+        protected override int GetDefense()
+        {
+            return defense;
+        }
+
+        protected override int GetDamage()
+        {
+            return damage;
+        }
+
+        protected override void ReduceHp()
+        {
+            hp -= CurrentDamage;
+        }
+
+        protected abstract void PlayTurn();
+
+        public override void TurnStarted(int turnId)
+        {
+            if (id != turnId) return;
+            MyTurn = true;
+            PlayTurn();
+        }
+    }
+}
