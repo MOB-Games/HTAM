7fd025ab412e51623c0f1f63410a668def6f0d62
MMirbach <m.mirbach153@gmail.com>
2022-06-22 20:46:40 +0300

    d

--- Assets/Scripts/Core/SkillsAndConditions/Skill.cs
+++ Assets/Scripts/Core/SkillsAndConditions/Skill.cs
@@ -62,6 +62,9 @@ namespace Core.SkillsAndConditions
                 throw new ConstraintException($"{name}: Skill can't have a chance to inflict but not have a condition to inflict");
             if (energyCost > 0 && hpCost > 0)
                 throw new ConstraintException($"{name}: Skill can't cost both energy and hp");
+            if (conditionGo != null && conditionGo.GetComponent<Condition>().parametersPerLevel.Count !=
+                parametersPerLevel.Count)
+                throw new ConstraintException($"{name}: Mismatch between skill levels and condition levels");
         }
 
         private void Start()
