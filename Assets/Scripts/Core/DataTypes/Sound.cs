6e365ca27c9ff4de555e0a3f82e23c0c6e4eda05
MMirbach <m.mirbach153@gmail.com>
2022-08-25 18:51:07 +0300

    rat and wolf sounds

--- Assets/Scripts/Core/DataTypes/Sound.cs
+++ Assets/Scripts/Core/DataTypes/Sound.cs
@@ -7,7 +7,7 @@ public class Sound
     public AudioClip audioClip;
     [Range(0f,1f)]
     public float volume;
-    [Range(0f,1f)]
+    [Range(-3f,3f)]
     public float pitch;
     public bool loop;
 
